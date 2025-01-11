e5984b9bdc4f3da9ed36d3837f88405e9971a2d8
AndrewAntepa <andrewantepa@gmail.com>
2025-01-12 02:35:11 +0800

    hw4

---
+++ src/org/skypro/skyshop/exception/BestResultNotFoundException.java
@@ -0,0 +1,10 @@
+package org.skypro.skyshop.exception;
+
+import java.io.IOException;
+
+public class BestResultNotFoundException extends IOException {
+
+    public BestResultNotFoundException(String message) {
+        super(message);
+    }
+}
